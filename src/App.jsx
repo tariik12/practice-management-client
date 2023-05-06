bdf3991cb72fadbf6a4bd71f6ec987078c4ab952
tarikul-islam <tariikul12@gmail.com>
2023-05-06 15:49:17 +0600

    data load done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
- 
 
+  const [users, setUsers] = useState()
+  
+
+  console.log(users)
+  useEffect(() =>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data =>setUsers(data))
+  },[])
   return (
     <>
      <h1>practice management clint</h1>
-     
+
     </>
   )
 }
